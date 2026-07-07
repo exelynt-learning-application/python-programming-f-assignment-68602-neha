1aaa63ceeb67f0ee304c0ca81548ed3d05e7bc36
Akshay Khedkar <2134akshay@gmail.com>
2026-07-07 11:16:35 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,22 @@
+print("=== Python Syntax Structure Program ===")
+print("Welcome to Python")
+print("This program demonstrates an if statement.")
+
+number = 7
+
+if number % 2 != 0:
+    print("The number is odd.")print("=== Python Syntax Structure Program ===")
+print("Welcome to Python")
+print("This program demonstrates an if statement.")
+
+number = 7
+
+if number % 2 != 0:
+    print("The number is odd.")print("=== Python Syntax Structure Program ===")
+print("Welcome to Python")
+print("This program demonstrates an if statement.")
+
+number = 7
+
+if number % 2 != 0:
+    print("The number is odd.")
